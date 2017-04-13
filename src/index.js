1606d7a9c722f3b4954a37094139cded8424ccbd
Srinaath Ravichandran <sravichandran@deloitte.com>
2017-04-13 15:28:34 -0600

    More component updates

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
-/* eslint-disable no-console */
-console.log('hi');
+/*eslint-disable import/default */
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import {Provider} from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+
+//Webpack can import CSS files too!
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+  <Router history={browserHistory} routes={routes} />, document.getElementById('app')
+);
