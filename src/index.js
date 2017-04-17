7bf28f4d1a0881ef321a259fef479c08eedcab31
Srinaath Ravichandran <sravichandran@deloitte.com>
2017-04-17 16:15:29 -0600

    NEEDS UNIT TESTING

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 /*eslint-disable import/default */
 import 'babel-polyfill';
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import {Provider} from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import {Router, browserHistory} from 'react-router';
+import configureStore from './store/configureStore';
 import routes from './routes';
-
+import {loadCourses} from './actions/courseActions';
+import {loadAuthors} from './actions/authorActions';
 
 //Webpack can import CSS files too!
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/toastr/build/toastr.min.css';
+
+const store = configureStore();
+store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
-  <Router history={browserHistory} routes={routes} />, document.getElementById('app')
-);
+  <Provider store={store}>
+  <Router history={browserHistory} routes={routes}/>
+</Provider>, document.getElementById('app'));
